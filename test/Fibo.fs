5b03a994e17274073f6fb2229932220e6507ce45
mvdcam <57500777+mvdcam@users.noreply.github.com>
2022-11-21 13:23:55 +0100

    Fibo.fs mais unexpected token ?

---
+++ test/Fibo.fs
@@ -0,0 +1,20 @@
+BEGIN Fibonacci
+
+%% Program to display the Fibonacci sequence up to n-th term %%
+
+  READ(nterms) ,              :: Read a nterms number from user input
+  n1 := 0 ,
+  n2 := 1 ,
+  count := 0 ,
+
+IF (nterms = 1) THEN
+    PRINT(n1) ,
+  END ,
+  ELSE
+  WHILE (count < nterms) DO 
+    PRINT(n1) ,
+    temp := n1 + n2 ,    :: add the two previous number
+    n1 := n2 ,
+    n2 := temp ,         :: Update the values
+    count := count + 1 ,
+  END 

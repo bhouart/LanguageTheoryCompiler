ddaa72a96432d2b8c463750403218b554c0fc54e
mvdcam <57500777+mvdcam@users.noreply.github.com>
2022-12-16 14:35:04 +0100

    oups le gros tas

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -34,7 +34,9 @@ public class CodeGenerator {
             %1 = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.strR, i32 0, i32 0), i32* %x)
             %2 = load i32, i32* %x, align 4
             ret i32 %2
-        }""";
+        }
+        
+        declare i32 @__isoc99_scanf(i8*, ...) #1""";
     }
 
     public void generate() throws Exception {

9f6f14bca49383db7e99f0de95a2dbdf99bfd209
bhouart <56756819+bhouart@users.noreply.github.com>
2022-12-22 17:52:35 +0100

    commentaire

--- src/CodeGenerator.java
+++ src/CodeGenerator.java
@@ -36,7 +36,7 @@ public class CodeGenerator {
 
         """
         @.strR = private unnamed_addr constant [3 x i8] c\"%d\\00\", align 1
-        ; Function Attrs: nounwind uwtable
+
         define i32 @readInt() #0 {
             %x = alloca i32, align 4
             %1 = call i32 (i8*, ...) @__isoc99_scanf(i8* getelementptr inbounds ([3 x i8], [3 x i8]* @.strR, i32 0, i32 0), i32* %x)
